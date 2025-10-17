95dccbba529a1254c26c23b1badbf7388619abce
Carlos Steven Vargas Hernandez <steven17.v@gmail.com>
2025-10-17 14:01:20 -0500

    Cobertura pruebas unitarias por encima de 80

--- src/test/java/com/nexossoftware/pruebanexos/service/implementation/TransactionServiceTest.java
+++ src/test/java/com/nexossoftware/pruebanexos/service/implementation/TransactionServiceTest.java
@@ -1,6 +1,7 @@
 package com.nexossoftware.pruebanexos.service.implementation;
 
 import com.nexossoftware.pruebanexos.dto.CardDtoIn;
+import com.nexossoftware.pruebanexos.dto.MessageDtoOut;
 import com.nexossoftware.pruebanexos.dto.TransactionDtoOut;
 import com.nexossoftware.pruebanexos.exception.ElementNotFoundException;
 import com.nexossoftware.pruebanexos.exception.ErrorGeneralException;
@@ -23,8 +24,7 @@ import java.util.Date;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -60,6 +60,37 @@ class TransactionServiceTest {
 
 
     TransactionDtoOut result = transactionService.makePurchase(cardDtoIn);
+    assertEquals(Constantes.MESSAGE_OK, result.getResult());
+  }
+
+  @Test
+  void purchaseAnnulationOk() throws ElementNotFoundException, ErrorGeneralException {
+    CardDtoIn cardDtoIn = new CardDtoIn();
+    cardDtoIn.setCardId("2020201387691302");
+    cardDtoIn.setPrice(200L);
+    cardDtoIn.setTransactionId(1L);
+
+    Calendar calendar = Calendar.getInstance();
+    calendar.add(Calendar.DAY_OF_MONTH, 1);
+    Date tomorrow = calendar.getTime();
+
+    CardEntity cardEntity = new CardEntity();
+    cardEntity.setStatus(CardStatusEnum.ACTIVE.name());
+    cardEntity.setCardTypeId(CardTypeEnum.DEBIT.name());
+    cardEntity.setBalance(1000L);
+    cardEntity.setExpirationDate(tomorrow);
+
+    TransactionEntity transactionEntity = new TransactionEntity();
+    transactionEntity.setId(1L);
+    transactionEntity.setPrice(cardDtoIn.getPrice());
+    transactionEntity.setCardNumber(cardDtoIn.getCardId());
+    transactionEntity.setStatus(TransactionStatusEnum.APPROVED.name());
+    transactionEntity.setCard(cardEntity);
+    transactionEntity.setTransactionDate(new Date());
+
+    when(transactionRepository.findByIdAndCardNumberApproved(anyLong(), anyString())).thenReturn(Optional.of(transactionEntity));
+
+    MessageDtoOut result = transactionService.purchaseAnnulation(cardDtoIn);
     assertEquals(Constantes.MESSAGE_OK, result.getResult());
   }
 
